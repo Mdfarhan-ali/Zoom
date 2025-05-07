b12f27e7a23257789f589847d16bc60efd6be569
Mdfarhan-ali <farhanali4953@gmail.com>
2025-05-07 13:51:51 +0530

    Edited files

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,10 +14,15 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 
-app.set("port", (process.env.PORT || 8000))
+app.set("port", (process.env.PORT || 3000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+  
 
 app.use("/api/v1/users", userRoutes);
 
@@ -27,7 +32,7 @@ const start = async () => {
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+        console.log("LISTENIN ON PORT 3000")
     });
 
 
